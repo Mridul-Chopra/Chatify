2d41a1b88815dacba503d586d9f6b2bf3fb57c9d
Mridul Chopra <mridul.chopra@paxcel.net>
2020-06-29 16:42:46 +0530

    redux implementation

--- server/chat/chat.js
+++ server/chat/chat.js
@@ -1,22 +1,37 @@
-
+const hash = require('object-hash');
 
 module.exports = (app,io)=>{
 
-   
+    var users = [];
+    var connections =[];
+	
+	app.get('/',(req,res)=>{
+		res.json('Yes its working');
+	})
 
-    app.get('/',(req,res)=>{
-        res.sendFile(__dirname+'/index.html');
-    });
-
-    io.on('connection',(socket)=>{
-        console.log('connected');
-
-        socket.on('message',(data)=>{
-            console.log('message received : ',data);
-            io.sockets.emit('received-message',data);
+    io.on('connection',(socket)=>{ // connect to the socket
+        socket.on('message',(data)=>{ // on some message is received
+            io.sockets.emit('received-message',data); // emit new event broadcasting the message
         })
 
+        socket.on('newUser',(data)=>{  // whenever new user is connected
+            let  id = hash(socket);
+            users.push(data); // push the user to the list of users
+            connections.push(socket); // push the socket in the connections
+
+            io.sockets.emit('users',users); // send the updated information to all the clients
+        });
+
+        socket.on('disconnect',(data)=>{
+            let index = connections.indexOf(socket); // get the index of the connection and the user
+            connections.splice(index,1); // remove the connections from the list
+            users.splice(index,1); // remove the user from the list
+
+            io.sockets.emit('users',users); // send the updated information to all the clients
+        });
     })
+
+
 
     
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
+/* requiring all the dependencies for the module */
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io').listen(server);
+const chat = require('./chat/chat'); // requiring the chat module 
 
+const app = express(); // making the express app
+const server = http.createServer(app); // creating the http server
+const io = socketio.listen(server); //getting the socket.io server ready
 
-server.listen(5000);
-const chat = require('./chat/chat');
-chat(app,io );
+server.listen(process.env.PORT || 5000 ,()=>{console.log('listening to port 5000')}); // listen to port 5000 
+
+chat(app,io); //making the chat application ready for work
 
